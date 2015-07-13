d338309c29b4c9264c6d6482d2264ecf74390fb5
Ansel Rosenberg <ansel01@gmail.com>
2015-07-12 19:50:18 -0700

    Add basic express server requirements

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+module.exports = app;
